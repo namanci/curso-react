6b46dc30086ea937de514ebba2e152f86c748913
namanci <namanci@gmail.com>
2023-10-29 23:36:17 -0300

    context + fbConfig

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import './styles.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetail from './components/ItemDetail/ItemDetail';
+import { CartProvider } from './context/CartContext';
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
-        <Routes>
-          <Route exact path="/" element={<ItemListContainer title={'Bienvenido'}/>} />
-          <Route exact path="/categoria/:catid" element={<ItemListContainer title={'Productos'} />} />
-          <Route path="/producto/:id" element={<ItemDetail />} />
-        </Routes>
+      <CartProvider>
+        <NavBar />
+          <Routes>
+            <Route exact path="/" element={<ItemListContainer title={'Bienvenido'}/>} />
+            <Route exact path="/categoria/:catid" element={<ItemListContainer title={'Productos'} />} />
+            <Route path="/producto/:id" element={<ItemDetail />} />
+          </Routes>
+      </CartProvider>
     </BrowserRouter>
   );
 }
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import ItemCount from "../ItemCount/ItemCount";
 import productosJSON from '../../products.json';
+import { useCart } from "../../context/CartContext";
 
 const ItemDetail = () => {
     const { id } = useParams()
+    const { addToCart } = useCart()
     const [item, setProduct] = useState(null)
+    const [quantityAdded, setQuantityAdded] = useState(0)
+    
+    const handleAdded = (quantity) => {
+        setQuantityAdded(quantity)
+        addToCart(item, quantity)
+    }
 
     useEffect(() => {
         setTimeout(() => {
@@ -29,13 +37,21 @@ const ItemDetail = () => {
                 </div>
                 <div>
                     <div className="uk-card-body">
-                        <div class="uk-card-badge uk-label">{item.subcategoria}</div>
+                        <div className="uk-card-badge uk-label">{item.subcategoria}</div>
                         <h2 className="uk-card-title">{item.nombre}</h2>
                         <p>{item.descripcion}</p>
                         <p><b>Especificaciones:</b> {item.especificaciones_tecnicas}</p>
                         <p><b>Precio:</b> US${item.precio}</p>
                         <br></br>
-                        <ItemCount initial={1} stock={25} onAdd={(quantity) => console.log('Cantidad ', quantity)}/>
+                        {
+                            quantityAdded > 0 ? (
+                                <div className="uk-flex uk-flex-center">
+                                    <Link to="/cart" className="uk-button uk-button-default uk-button-large">Finalizar compra</Link>
+                                </div>
+                            ) : (
+                                <ItemCount initial={1} stock={25} onAdd={handleAdded}/>
+                            )
+                        }
                     </div>
                 </div>
             </div>
